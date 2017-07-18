a2dbb6eb4a2b59622465844312747603f7295e66
nguyenvantra <dkdarknessnvt@gmail.com>
2017-07-19 01:51:49 +0700

    add view content

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import {
     StyleSheet,
     Text,
     View,
-    Platform
+    Platform,
+    Slider,
+    TextInput
 } from 'react-native';
 
 export default class App extends Component {
@@ -12,6 +14,37 @@ export default class App extends Component {
             <View style={styles.container}>
                 <View style={styles.header}>
                     <Text style={styles.headerText}>Color Picker</Text>
+                </View>
+
+                <View style={styles.body}>
+                    <View style={styles.wrapper}>
+                        <View style={styles.wrapperTop}>
+                            <View style={styles.component}>
+                                <Text>R</Text>
+                                <Slider style={styles.slider} />
+                                <View>
+                                    <TextInput underlineColorAndroid="transparent" style={styles.textInput} />
+                                </View>
+                            </View>
+
+                            <View style={styles.component}>
+                                <Text>G</Text>
+                                <Slider style={styles.slider} />
+                                <View>
+                                    <TextInput underlineColorAndroid="transparent" style={styles.textInput} />
+                                </View>
+                            </View>
+
+                            <View style={styles.component}>
+                                <Text>B</Text>
+                                <Slider style={styles.slider} />
+                                <View>
+                                    <TextInput underlineColorAndroid="transparent" style={styles.textInput} />
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.wrapperBottom}></View>
+                    </View>
                 </View>
             </View>
         );
@@ -38,11 +71,54 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#000',
         ...Platform.select({
-            ios:{
-                marginTop:15
+            ios: {
+                marginTop: 15
             },
-            android:{
-                marginTop:0
+            android: {
+                marginTop: 0
+            }
+        })
+    },
+    body: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    wrapper: {
+        width: 300,
+        height: 350
+    },
+    wrapperTop: {
+        flex: 1
+    },
+    wrapperBottom: {
+        flex: 1,
+        backgroundColor: 'red'
+    },
+    component: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10
+    },
+    slider: {
+        flex: 1,
+        marginLeft: 5,
+        marginRight: 5
+    },
+    textInput: {
+        width: 50,
+        height: 30,
+        borderColor: 'gray',
+        borderWidth: 1,
+        textAlign: 'center',
+        ...Platform.select({
+            ios: {
+                paddingBottom: 0
+            },
+            android: {
+                paddingBottom: 6
             }
         })
     }
